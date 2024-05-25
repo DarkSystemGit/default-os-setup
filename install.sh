78f7bc5ebc705f791ce0eaa010091a8aba68b07b
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-05-25 16:43:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt install flatpak wget git gnome-software-plugin-flatpak gnome-browser-connector gnome-software gnome-extensions -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.github.GradienceTeam.Gradience -y
+flatpak run --command=gradience-cli com.github.GradienceTeam.Gradience import -p ./simple-gnome.json
+cd /tmp
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
@@ -28,5 +30,15 @@ gnome-shell-extension-installer 3193
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+wget https://github.com/quickemu-project/quickemu/releases/download/4.9.4/quickemu_4.9.4-1_all.deb
+sudo apt-get install ./quickemu_4.9.4-1_all.deb
+mkdir ~/VMs
+cd ~/VMs
+quickget macos ventura
+cd /tmp
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.3.1.19/android-studio-2023.3.1.19-linux.tar.gz
+sudo tar -xvf android-studio-2023.3.1.19-linux.tar.gz -C /usr/local/ 
+sudo ln -s /usr/local/android-studio/bin/studio.sh /usr/bin/android-studio
+sudo chmod +x /usr/bin/android-studio
 git config --global user.name "DarkSystemGit"
 git config --global user.email "85317259+DarkSystemGit@users.noreply.github.com"
